d4d851e959c19ceceadf3e7c5b113c0a97dd5894
Doniyor <donyor6hamidov25@gmail.com>
2025-10-08 15:40:54 +0500

    static and html

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', home_view, name='home'),
+    path('clubs/', clubs_view, name='clubs'),
+    path('latest_transfers/', latest_transfers_view, name='latest_transfers'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from transfer.models import Transfer
+from main.models import *
+from transfer.models import *
+
+
+def home_view(request):
+    return render(request, 'index.html')
+
+def clubs_view(request):
+    clubs = Club.objects.all()
+    context = {
+        'clubs': clubs
+    }
+    return render(request, 'clubs.html', context)
+
+def latest_transfers_view(request):
+    last_season= Season.objects.order_by('name').last()
+    transfers = Transfer.objects.filter(season=last_season).order_by('-price')
+    context = {
+        'transfers': transfers,
+        'last_season': last_season
+
+    }
+    return render(request, 'latest-transfers.html', context)
